02c39833bc971cdf1d2bf686aaa364bd89438c3a
HedaetShahriar <shahriahedaet@gmail.com>
2025-07-14 04:10:29 +0600

    Sora and Inter font added

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,6 +3,6 @@ import { createBrowserRouter } from "react-router";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <div className="text-6xl">Hello World</div>,
   },
 ]);
